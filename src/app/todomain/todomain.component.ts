17baba99c6f4f025b9e781966f1fdf2260693cba
thienn <thien.oslo@gmail.com>
2018-08-13 18:17:27 +0200

    Rebuilt the main service for todolist + the CRUD operations related to it
    + Recreating the main list of items. Via services to connect to Firebase. Then Angular & Typescript to take care of the rest.
    ---
    TODO:
    + Update some older legacy syntaxes code to work with the newest.
    + Create the model for item, so the arrays only accept a specific type. For safety reasons when working between different frontend codebases
    + Admin section for editing etc

---
+++ src/app/todomain/todomain.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
+
+@Component({
+  selector: 'app-todomain',
+  templateUrl: './todomain.component.html',
+  styleUrls: ['./todomain.component.css'],
+  providers: [TodoService], // To make it local for this component for now to avoid problems. Will make it global later when the rest of the features are ready
+})
+export class TodomainComponent implements OnInit {
+  todolistArray: any[]; // Change later to only accept a certain model structure, for safety reasons. Between ionic and angular
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    this.todoService.getTodoList().snapshotChanges().subscribe(
+      item => {
+        this.todolistArray = [];
+        // loop through the list given from the firebase, then push them to the todolistArray for working in local 
+        item.forEach(element => {
+          var localItem = element.payload.toJSON(); // Legacy will change to updated version, as it's taken as JSON by default
+          localItem["$key"] = element.key; // assign the unique key to the element as key variable
+          this.todolistArray.push(localItem); // Then push it to the array
+        })
+
+        // Sort the list based on isChecked false -> true (so all the unchecked are on top and then the finished checked ones are on bottom)
+        this.todolistArray.sort((a, b) => {
+          return a.isChecked - b.isChecked;
+        })
+      }
+    )
+  }
+
+  onAdd(itemName) {
+    console.log(itemName.value);
+    this.todoService.addItem(itemName.value); // Access the itemName value (whatever is typed into input field, then add it to the firebase via service)
+    itemName.value = null; // Reset the input field
+  }
+
+  updateCheck($key: string, isChecked) {
+    this.todoService.checkItemStatus($key, !isChecked); // Change the isChecked based on what it currently is, so if it's false then change to true, true then false
+  }
+
+  onDelete($key: string) {
+    this.todoService.removeItem($key);
+  }
+}
